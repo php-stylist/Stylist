68cad9aa6b0331d8d5236fb6021de4da44d32c33
Jiří Pudil <me@jiripudil.cz>
2019-04-28 10:49:29 +0200

    QA: fix box build

--- scoper.inc.php
+++ scoper.inc.php
@@ -3,6 +3,19 @@
 return [
 	'prefix' => null,
 	'finders' => [],
+	'patchers' => [
+		static function (string $filePath, string $prefix, string $content): string {
+			if (strpos($filePath, 'vendor/nette/di/src/DI/Extensions/ParametersExtension.php') !== false) {
+				return preg_replace(
+					'/addBody\(\'Nette\\\\\\\\Utils\\\\\\\\Validators/',
+					'addBody(\'' . $prefix . '\\\\\\\\Nette\\\\\\\\Utils\\\\\\\\Validators',
+					$content
+				);
+			}
+
+			return $content;
+		},
+	],
 	'whitelist' => [
 		'Stylist\*',
 	],
